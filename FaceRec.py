7be381c5ac0ca38dab2f3d16190c078db2090689
Siyu Chen <44207825+ryanyyyy11@users.noreply.github.com>
2021-01-31 03:44:46 -0500

    finished facial control

---
+++ FaceRec.py
@@ -0,0 +1,28 @@
+import cv2
+from math import sin, cos, radians
+
+settings = {
+    'scaleFactor': 1.3, 
+    'minNeighbors': 3, 
+    'minSize': (50, 50), 
+    'flags': cv2.CASCADE_FIND_BIGGEST_OBJECT|cv2.CASCADE_DO_ROUGH_SEARCH
+}
+
+def rotate_image(image, angle):
+    if angle == 0: return image
+    height, width = image.shape[:2]
+    rot_mat = cv2.getRotationMatrix2D((width/2, height/2), angle, 0.9)
+    result = cv2.warpAffine(image, rot_mat, (width, height), flags=cv2.INTER_LINEAR)
+    return result
+
+def rotate_point(pos, img, angle):
+    if angle == 0: return pos
+    x = pos[0] - img.shape[1]*0.4
+    y = pos[1] - img.shape[0]*0.4
+    newx = x*cos(radians(angle)) + y*sin(radians(angle)) + img.shape[1]*0.4
+    newy = -x*sin(radians(angle)) + y*cos(radians(angle)) + img.shape[0]*0.4
+    return int(newx), int(newy), pos[2], pos[3]
+
+
+
+cv2.destroyWindow("facedetect")
--- main.py
+++ main.py
@@ -3,25 +3,33 @@ import sys
 from pygame.locals import *
 import random
 import buttons
+import FaceRec
 from pygame import mixer 
+import cv2
+import base64
 
 pygame.init() 
-
+face = cv2.CascadeClassifier('img/frontface.xml')
+eye = cv2.CascadeClassifier('img/eye.xml')
+smile = cv2.CascadeClassifier('img/smile.xml')
 class GatorJump:
     def __init__(self):
-        self.screen = pygame.display.set_mode((600, 800))
-        self.screen_length = 600
-        self.screen_width = 800
-        self.background = pygame.transform.scale(pygame.image.load("img/bg2.jpg"), (600, 800))
+        self.screen = pygame.display.set_mode((560, 720))
+        self.screen_length = 560
+        self.screen_width = 720
+        self.background = pygame.transform.scale(pygame.image.load("img/bg2.jpg"), (560, 720))
         self.player = pygame.transform.scale(pygame.image.load("img/gatorRight.png"), (80, 100))
         self.platformStation = pygame.transform.scale(pygame.image.load("img/platform.png"), (75, 15))
         self.plane = pygame.transform.scale(pygame.image.load("img/plane-left.png"), (75, 15))
         self.spring = pygame.transform.scale(pygame.image.load("img/spring.png"), (15, 10))
-        self.playerX = 250
-        self.playerY = 600
+        self.playK = False
+        self.playF = False
+        self.playerX = 200
+        self.playerY = 520
+        self.faceX = 0
         self.jump = 0
         self.gravity = 0
-        self.platforms = [[250, 700, 0]]
+        self.platforms = [[200, 620, 0]]
         self.springs = []
         self.cameray = 0
         self.xmovement = 0
@@ -31,12 +39,34 @@ class GatorJump:
         self.player_died = False
         self.in_main_menu = True
         self.in_settings = False
-        self.play_button = buttons.buttons((255, 255, 255), self.screen_width/2 - 200, (self.screen_length*0.8), 200, 100, "Play!")
+        self.playK_button = buttons.buttons((255, 255, 255), self.screen_width/2 - 290, (self.screen_length*0.625), 420, 80, "Play With Keyboard")
+        self.playF_button = buttons.buttons((255, 255, 255), self.screen_width/2 - 290, (self.screen_length*0.8), 420, 80, "Play With Face")
         self.menuMusic = mixer.Sound('img/menuMusic.wav')
-        self.return_to_menu = buttons.buttons((255, 255, 255), self.screen_width/2 - 285, (self.screen_length*0.8), 350, 100, "Back to Main Menu")
-        self.change_background = buttons.buttons((255, 255, 255), self.screen_width/2 - 300, (self.screen_length), 400, 100, "Change Background")
+        self.return_to_menu = buttons.buttons((255, 255, 255), self.screen_width/2 - 250, (self.screen_length*0.7), 350, 100, "Back to Main Menu")
+        self.change_background = buttons.buttons((255, 255, 255), self.screen_width/2 - 290, (self.screen_length*0.975), 420, 80, "Change Background")
 
-    def updatePlayer(self):
+
+    def updatePlayer_Face(self):
+        self.player = pygame.transform.scale(pygame.image.load("img/gatorRight.png"), (80, 100))
+        if not self.jump:
+            self.playerY += self.gravity
+            if(self.gravity < 10):
+                self.gravity += 0.5
+        elif self.jump:
+            self.playerY -= self.jump
+            self.jump -= 1
+        self.playerX = self.faceX
+        if self.playerX >= 480:
+            self.playerX = 480
+        elif self.playerX <= 0:
+            self.playerX = 0
+        if(self.playerY - self.cameray <= 275):
+            self.cameray -= 8
+            self.score_value += 1
+        
+        self.screen.blit(self.player, (self.playerX, self.playerY - self.cameray))
+
+    def updatePlayer_key(self):
         if not self.jump:
             self.playerY += self.gravity
             if(self.gravity < 10):
@@ -58,12 +88,12 @@ class GatorJump:
                 self.xmovement -= 1
             elif self.xmovement < 0:
                 self.xmovement += 1
-        if self.playerX >= 525:
-            self.playerX = 525
+        if self.playerX >= 480:
+            self.playerX = 480
         elif self.playerX <= 0:
             self.playerX = 0
         self.playerX += self.xmovement
-        if(self.playerY - self.cameray <= 300):
+        if(self.playerY - self.cameray <= 275):
             self.cameray -= 8
             self.score_value += 1
         self.screen.blit(self.player, (self.playerX, self.playerY - self.cameray))
@@ -72,7 +102,7 @@ class GatorJump:
         for p in self.platforms:
             rect = pygame.Rect(p[0]+10, p[1], self.platformStation.get_width()-20, self.platformStation.get_height()-5)
             player = pygame.Rect(self.playerX+10, self.playerY+70, self.player.get_width()-20, self.player.get_height()-73)
-            if rect.colliderect(player) and self.gravity and self.playerY < (p[1] - self.cameray) and ((p[1] - self.cameray) < 785):
+            if rect.colliderect(player) and self.gravity and self.playerY < (p[1] - self.cameray) and ((p[1] - self.cameray) < 705):
                 jumpSound = mixer.Sound('img/jump.wav')
                 jumpSound.set_volume(0.12)
                 jumpSound.play(0)
@@ -128,9 +158,9 @@ class GatorJump:
             
 
     def generatePlatform(self):
-        on = 800
-        while on > -100:
-            x = random.randint(0,525)
+        on = 700
+        while on > -150:
+            x = random.randint(0,450)
             platform = random.randint(0,1000)
             if platform < 850:
                 platform = 0
@@ -146,14 +176,14 @@ class GatorJump:
     def die(self):
         # error: doesnt print message - fixed
         self.player_died = True
-        message = self.font.render("You died...", True, (255, 255, 255))
+        message = self.font.render("You Died...", True, (255, 255, 255))
         message2 = self.font.render("Your Score is: " + str(self.score_value), True, (255, 255, 255))
         #message3 = self.font.render("Press ESC to go back to Main Menu", True, (255, 255, 255))
         message4 = self.font.render("Press Any Key to Play Again!", True, (255, 255, 255))
-        self.screen.blit(message, (self.screen_length*0.35, self.screen_width*0.35))
-        self.screen.blit(message2, (self.screen_length*0.27, self.screen_width*0.4))
+        self.screen.blit(message, (self.screen_length*0.33, self.screen_width*0.35))
+        self.screen.blit(message2, (self.screen_length*0.25, self.screen_width*0.4))
         #self.screen.blit(message3, (self.screen_length*0.01, self.screen_width*0.45))
-        self.screen.blit(message4, (self.screen_length*0.1, self.screen_width*0.45))
+        self.screen.blit(message4, (self.screen_length*0.07, self.screen_width*0.45))
         self.return_to_menu.draw_button(self.screen, 40, (0,0,0))
         pygame.display.update()     #adding this prints message!!!
         while True:
@@ -165,6 +195,8 @@ class GatorJump:
                 mouse_pos = pygame.mouse.get_pos()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.return_to_menu.is_over(mouse_pos):
+                        self.playK = False
+                        self.playF = False
                         self.in_main_menu = True
                         return
                 if event.type == pygame.KEYDOWN:
@@ -192,7 +224,8 @@ class GatorJump:
         self.screen.fill((64, 174, 118))
         title = self.font.render("Welcome to Gator Jump!", True, (255, 255, 255))
         self.screen.blit(title, (self.screen_width*0.11, self.screen_length*0.35))
-        self.play_button.draw_button(self.screen, 40, (0,0,0))
+        self.playK_button.draw_button(self.screen, 40, (0,0,0))
+        self.playF_button.draw_button(self.screen, 40, (0,0,0))
         self.change_background.draw_button(self.screen, 40, (0,0,0))
         pygame.display.update()
         #pygame.draw.rect(self.screen, (0,0,0), (round(self.screen_width - 2, self.screen_length - 2), 
@@ -213,17 +246,48 @@ class GatorJump:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.play_button.is_over(mouse_pos):
+                    if self.playK_button.is_over(mouse_pos):
                         self.in_main_menu = False
+                        self.playK = True
                         self.menuMusic.stop()
                         mixer.music.load('img/music1.wav')
                         mixer.music.set_volume(0.3)
                         mixer.music.play(-1)
                         return  
+                    if self.playF_button.is_over(mouse_pos):
+                        self.in_main_menu = False
+                        self.playF = True
+                        self.menuMusic.stop()
+                        mixer.music.load('img/music1.wav')
+                        mixer.music.set_volume(0.3)
+                        mixer.music.play(-1)
+                        return 
                     if self.change_background.is_over(mouse_pos):
                         self.in_settings = True
                         self.settings_menu()
                         return
+        pass
+
+    def runF_Func(self, camera):
+        ret, img = camera.read()
+        img = cv2.rotate(img,cv2.ROTATE_90_COUNTERCLOCKWISE)
+        roi = img[320:880, 0:720]
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        for angle in [0, -25, 25]:
+            rimg = FaceRec.rotate_image(roi, angle-90)
+            detected = face.detectMultiScale(rimg, **FaceRec.settings)
+            if len(detected):
+                detected = [FaceRec.rotate_point(detected[-1], roi, -angle+90)]
+                break
+        for x, y, w, h in detected[-1:]:
+            cv2.rectangle(roi, (x, y), (x+w, y+h), (255,255,0), 2)
+            self.faceX = y+int(h/2)
+
+        pygame.surfarray.blit_array(self.screen,roi)
+
+    def runK_Func(self):
+        self.screen.fill((255,255,255))
+        self.screen.blit(self.background, (0,0))
         
     def run(self):
         #Set title and icon
@@ -231,16 +295,22 @@ class GatorJump:
         pygame.display.set_icon(pygame.image.load('img/gatorIcon.png'))
 
         self.main_menu()
+        camera =  cv2.VideoCapture(0)
+        camera.set(3, 1280)
+        camera.set(4, 720)
         
-        
-
         clock = pygame.time.Clock()
         self.generatePlatform()
         #Game Loop
         while True:
-            self.screen.fill((255,255,255))
-            self.screen.blit(self.background, (0,0))
-            clock.tick(60)
+            if(self.playF):
+                self.runF_Func(camera)
+                clock.tick(80)
+            elif(self.playK):
+                self.runK_Func()
+                clock.tick(50)
+           
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
@@ -258,9 +328,13 @@ class GatorJump:
             if self.in_main_menu:
                 self.main_menu()
             self.drawPlatform()
-            self.updatePlayer()
+            if(self.playK):
+                self.updatePlayer_key()
+            if(self.playF):
+                self.updatePlayer_Face()
             self.showScore()
             self.updatePlatform()
             pygame.display.flip()
 
 GatorJump().run()
+cv2.destroyWindow("facedetect")
